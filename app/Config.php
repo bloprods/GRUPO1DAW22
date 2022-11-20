8564ef211dd26d97be6b300625a0e98ea2d1f12d
Angel <martinez.reina.angel.fernando@iescamas.es>
2022-11-20 18:45:22 +0100

    Cambios fichero

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentosx2022";
+     static public $mvc_bd_usuario  = "mygrupo1tr00";
+     static public $mvc_bd_clave    = "BTtrF4Va";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
